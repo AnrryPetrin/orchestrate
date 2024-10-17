e9268947d5a1645f121b080be6875baf08aab738
Anrry Petrin De Araujo <108158140+AnrryPetrin@users.noreply.github.com>
2024-10-17 20:20:47 -0300

    Refactor ProjetoRepository to add new methods for querying projects by date range and fetching associated employees

--- src/main/java/com/anrry/orchestrate/modules/projeto/ProjetoRepository.java
+++ src/main/java/com/anrry/orchestrate/modules/projeto/ProjetoRepository.java
@@ -1,5 +1,13 @@
 package com.anrry.orchestrate.modules.projeto;
 
-public class ProjetoRepository {
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjetoRepository extends JpaRepository<Projeto, Integer> {
+  List<Projeto> findAllByDataInicioBetween(String dataInicio, String dataFim);
+
+  Projeto findByIdAndFetchFuncionarios(Integer id);
 }

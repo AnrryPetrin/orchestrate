800e06070279be9c3d433d327837df9351aa2f0f
Anrry Petrin De Araujo <108158140+AnrryPetrin@users.noreply.github.com>
2024-10-17 21:34:19 -0300

    Refactor ProjetoRepository to fetch funcionarios when finding by id

--- src/main/java/com/anrry/orchestrate/modules/projeto/ProjetoRepository.java
+++ src/main/java/com/anrry/orchestrate/modules/projeto/ProjetoRepository.java
@@ -3,11 +3,13 @@ package com.anrry.orchestrate.modules.projeto;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProjetoRepository extends JpaRepository<Projeto, Integer> {
   List<Projeto> findAllByDataInicioBetween(String dataInicio, String dataFim);
 
-  Projeto findByIdAndFetchFuncionarios(Integer id);
+  @Query("SELECT p FROM Projeto p LEFT JOIN FETCH p.funcionarios WHERE p.id = :id")
+  Projeto findByIdWithFuncionarios(Integer id);
 }

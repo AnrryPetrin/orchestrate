5e8b8a1c4c569485c3ac5e92acb880b3f174d2d1
Anrry Petrin De Araujo <108158140+AnrryPetrin@users.noreply.github.com>
2024-10-17 21:34:27 -0300

    Refactor SetorRepository to fetch funcionarios when finding all setores

--- src/main/java/com/anrry/orchestrate/modules/setor/SetorRepository.java
+++ src/main/java/com/anrry/orchestrate/modules/setor/SetorRepository.java
@@ -3,9 +3,9 @@ package com.anrry.orchestrate.modules.setor;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
 
-@Repository
 public interface SetorRepository extends JpaRepository<Setor, Integer> {
+  @Query("SELECT s FROM Setor s LEFT JOIN FETCH s.funcionarios")
   List<Setor> findAllWithFuncionarios();
 }

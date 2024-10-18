d242e9315d7f62a49914b8d7f9e5f88833c0667d
Anrry Petrin De Araujo <108158140+AnrryPetrin@users.noreply.github.com>
2024-10-17 21:50:59 -0300

    Refactor FuncionarioService to include fetching setor information by funcionario id

--- src/main/java/com/anrry/orchestrate/modules/funcionario/FuncionarioService.java
+++ src/main/java/com/anrry/orchestrate/modules/funcionario/FuncionarioService.java
@@ -9,41 +9,66 @@ import org.springframework.stereotype.Service;
 import com.anrry.orchestrate.modules.projeto.DadosProjetoDTO;
 import com.anrry.orchestrate.modules.projeto.Projeto;
 import com.anrry.orchestrate.modules.projeto.ProjetoRepository;
+import com.anrry.orchestrate.modules.setor.DadosSetorDTO;
 import com.anrry.orchestrate.modules.setor.Setor;
 import com.anrry.orchestrate.modules.setor.SetorRepository;
 
 @Service
 public class FuncionarioService {
-    @Autowired
-    private FuncionarioRepository funcionarioRepository;
-    @Autowired
-    private ProjetoRepository projetoRepository;
-    @Autowired
-    private SetorRepository setorRepository;
+  @Autowired
+  private FuncionarioRepository funcionarioRepository;
+  @Autowired
+  private ProjetoRepository projetoRepository;
+  @Autowired
+  private SetorRepository setorRepository;
 
-    public void adicionar(FuncionarioDTO funcionarioDTO) {
-        Funcionario funcionario = new Funcionario();
-        funcionario.setNome(funcionarioDTO.getNome());
-        if (funcionarioDTO.getProjetoId() != null) {
-            Projeto projeto = projetoRepository.findById(funcionarioDTO.getProjetoId()).orElse(null);
-            funcionario.setProjeto(projeto);
-        }
-        if (funcionarioDTO.getSetorId() != null) {
-            Setor setor = setorRepository.findById(funcionarioDTO.getSetorId()).orElse(null);
-            funcionario.setSetor(setor);
-        }
-        funcionarioRepository.save(funcionario);
+  public void adicionar(FuncionarioDTO funcionarioDTO) {
+    Funcionario funcionario = new Funcionario();
+    funcionario.setNome(funcionarioDTO.getNome());
+    if (funcionarioDTO.getProjetoId() != null) {
+      Projeto projeto = projetoRepository.findById(funcionarioDTO.getProjetoId()).orElse(null);
+      funcionario.setProjeto(projeto);
     }
+    if (funcionarioDTO.getSetorId() != null) {
+      Setor setor = setorRepository.findById(funcionarioDTO.getSetorId()).orElse(null);
+      funcionario.setSetor(setor);
+    }
+    funcionarioRepository.save(funcionario);
+  }
 
-    public List<DadosProjetoDTO> buscarProjetos(Integer idFuncionario) {
-        Funcionario funcionario = funcionarioRepository.findById(idFuncionario).orElseThrow(() -> new RuntimeException("Funcionário não encontrado"));
-        Projeto projeto = funcionario.getProjeto();
-        if (projeto != null) {
-            List<FuncionarioDTO> funcionarios = projeto.getFuncionarios().stream()
-                    .map(f -> new FuncionarioDTO(f.getNome(), f.getProjeto().getId(), f.getSetor() != null ? f.getSetor().getId() : null))
-                    .collect(Collectors.toList());
-            return List.of(new DadosProjetoDTO(projeto.getId(), projeto.getNome(), projeto.getDescricao(), funcionarios));
-        }
-        return List.of();
+  public FuncionarioDTO buscarFuncionarioPorId(Integer id) {
+    Funcionario funcionario = funcionarioRepository.findById(id)
+        .orElseThrow(() -> new RuntimeException("Funcionário não encontrado"));
+    return new FuncionarioDTO(funcionario.getNome(),
+        funcionario.getProjeto() != null ? funcionario.getProjeto().getId() : null,
+        funcionario.getSetor() != null ? funcionario.getSetor().getId() : null);
+  }
+
+  public List<DadosProjetoDTO> buscarProjetos(Integer idFuncionario) {
+    Funcionario funcionario = funcionarioRepository.findById(idFuncionario)
+        .orElseThrow(() -> new RuntimeException("Funcionário não encontrado"));
+    Projeto projeto = funcionario.getProjeto();
+    if (projeto != null) {
+      List<FuncionarioDTO> funcionarios = projeto.getFuncionarios().stream()
+          .map(f -> new FuncionarioDTO(f.getNome(), f.getProjeto().getId(),
+              f.getSetor() != null ? f.getSetor().getId() : null))
+          .collect(Collectors.toList());
+      return List.of(new DadosProjetoDTO(projeto.getId(), projeto.getNome(), projeto.getDescricao(), funcionarios));
     }
+    return List.of();
+  }
+
+  public DadosSetorDTO buscarSetor(Integer idFuncionario) {
+    Funcionario funcionario = funcionarioRepository.findById(idFuncionario)
+        .orElseThrow(() -> new RuntimeException("Funcionário não encontrado"));
+    Setor setor = funcionario.getSetor();
+    if (setor != null) {
+      List<FuncionarioDTO> funcionarios = setor.getFuncionarios().stream()
+          .map(f -> new FuncionarioDTO(f.getNome(), f.getProjeto() != null ? f.getProjeto().getId() : null,
+              f.getSetor().getId()))
+          .collect(Collectors.toList());
+      return new DadosSetorDTO(setor.getId(), setor.getNome(), funcionarios);
+    }
+    return null;
+  }
 }
